50fe34013b5508cccb8410fdbde54e28222dd300
Dalmasio Fernandes <dalmasiof@gmail.com>
2022-08-10 22:07:11 -0300

    fundamentasl, components and comments

---
+++ fundamentals/fundamentals/src/App.js
@@ -0,0 +1,18 @@
+//Components
+import './components/FirstComponent'
+//Styles
+import './App.css';
+import FirstComponent from './components/FirstComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Fundamentos React
+      </h1>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ fundamentals/fundamentals/src/components/FirstComponent.js
@@ -0,0 +1,14 @@
+//Creation
+const FirstComponent = ()=>{
+    return (
+        <div>
+            {/*component is allways envolt into a div*/}
+            <h1>
+                My first Component
+            </h1>
+        </div>
+    )
+}
+
+//Export
+export default FirstComponent;
