c7b4bec6d8fe6ad05552a70292748aa9f5e47509
Dalmasio Fernandes <dalmasiof@gmail.com>
2022-08-16 20:55:47 -0300

    input state manipulation

--- forms/src/Components/Form.js
+++ forms/src/Components/Form.js
@@ -1,4 +1,14 @@
+import{ useState} from "react"
+
 const Form = () => {
+    const [name,setName] = useState();
+    const [email,setEmail] = useState();
+
+    const onNameInpuChange=(event)=>{
+        console.log(event.target.value);
+        setName(event.target.value)
+    }
+
   return (
     <div>
       <h2>Forms</h2>
@@ -6,17 +16,25 @@ const Form = () => {
         <div>
           <label htmlFor="name">Name:</label>
         </div>
-        <input type="text" name="name" placeholder="Name"></input>
+        <input type="text" name="name" placeholder="Name" onChange={onNameInpuChange}></input>
         <label>
           <div>
             <span>Email</span>
           </div>
-          <input type="email" name="email" placeholder="Email"></input>
+          <input type="email" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}></input>
         </label>
         <div>
           <button type="submit">Submit Form</button>
         </div>
       </form>
+      <div>
+        <h2>
+            Name: {name}
+        </h2>
+        <h2>
+            Email: {email}
+        </h2>
+      </div>
     </div>
   );
 };
