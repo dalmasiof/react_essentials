11f7515b432aa7332a9d23288ea0d74a85fa94b4
Dalmasio Fernandes <dalmasiof@gmail.com>
2022-08-16 22:40:17 -0300

    starting Css rules

--- challenge-one/src/App.js
+++ challenge-one/src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="mainContainer">
       <Home></Home>
       <FormImc></FormImc>
     </div>
